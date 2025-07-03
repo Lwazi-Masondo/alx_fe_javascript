2b66c2e3c689dd3ad0b4938e997a5138a30654a8
Nolwazi Masondo <masondop96@gmail.com>
2025-07-04 01:31:34 +0200

    Implemented functions to display a random quote and to add new quotes called "showRandomQuote" and create "addQuote" Form respectively

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,1 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  const quoteDisplay = document.getElementById("quoteDisplay");
+  const newQuote = document.getElementById("newQuote");
+  const addQuoteBtn = document.getElementById("addQuoteBtn");
+
+  let quotes = [
+    {
+      text: "We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty.",
+      category: "Inspiration",
+    },
+    {
+      text: "When love is not madness it is not love.",
+      category: "Romantic",
+    },
+    {
+      text: "It does not do to dwell on dreams and forget to live.",
+      category: "Life",
+    },
+    {
+      text: "The fool doth think he is wise, but the wise man knows himself to be a fool.",
+      category: "Wisdom",
+    },
+    {
+      text: "Learn the rules like a pro, so you can break them like an artist.",
+      category: "Motivation",
+    },
+  ];
+
+  function addQuote() {
+    const newQuoteText = document.getElementById("newQuoteText");
+    const newQuoteCategory = document.getElementById("newQuoteCategory");
+
+    const textInput = newQuoteText.value;
+    const categoryInput = newQuoteCategory.value;
+
+    quotes.push({ text: textInput, category: categoryInput });
+
+    console.log(quotes);
+
+    newQuoteText.value = "";
+    newQuoteCategory.value = "";
+  }
+
+  function showRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex].text;
+    const quoteCategory = quotes[randomIndex].category;
+
+    quoteDisplay.textContent = randomQuote + " - " + quoteCategory;
+  }
+
+  addQuoteBtn.addEventListener("click", addQuote);
+  newQuote.addEventListener("click", showRandomQuote);
+});
